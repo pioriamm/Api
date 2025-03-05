808429a7839496cdb6541a9001df5a291095b534
marcelosantos <pioriam@gmail.com>
2025-03-05 18:46:44 -0300

    login

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19684b8baa41e5be309777a9a9729afca56f1eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8739dfdcfd4983020f83a3d043ead899d7ad6957")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
