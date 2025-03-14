ab632e349e22988990e3e5609cf3b1d0d92461fe
Marcelo Santos <pioriam@gmail.com>
2025-03-14 07:43:18 -0300

    config gitignore

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9a72c1823841cd96c980232f4ec2dd66039218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25cc3a3985aa27483d6ff4db210faa549a485dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
