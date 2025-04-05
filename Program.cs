e966baba894b03d99b40632515bade36da7081f8
Marcelo Santos <pioriam@gmail.com>
2025-04-05 12:22:26 -0300

    configuracao cors

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Api.Context;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,33 +8,37 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var stringConexaoBanco = builder.Configuration.GetConnectionString("Railway");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(stringConexaoBanco, ServerVersion.AutoDetect(stringConexaoBanco)));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(stringConexaoBanco, ServerVersion.AutoDetect(stringConexaoBanco)));
+
 
 builder.Services.AddControllers();
-builder.WebHost.UseUrls("http://0.0.0.0:5032");
 
-
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
-
-if (builder.Environment.IsProduction())
-{
-    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
-}
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        builder => builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
 });
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
-    app.UseCors("AllowAll");
-    app.UseHttpsRedirection();
+
+
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.MapControllers();
-    app.Run();
+}
+
+
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
