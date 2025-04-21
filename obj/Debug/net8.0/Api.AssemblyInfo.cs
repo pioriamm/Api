ed0b791517fc59772f3cc1088d9528d19894de90
Marcelo <pioriam@gmail.com>
2025-04-21 10:34:12 -0300

    Atualizacao projeto

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25cc3a3985aa27483d6ff4db210faa549a485dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e966baba894b03d99b40632515bade36da7081f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
